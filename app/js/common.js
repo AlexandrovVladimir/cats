6e1088626209afd9e732d0607bdb74d3bd6a3bbe
AlexandrovVladimir <beatable@mail.ru>
2019-03-08 16:28:55 +0200

    cats

--- app/js/common.js
+++ app/js/common.js
@@ -1,15 +1,46 @@
 $(document).ready(function() {
-  $('.cats-list__item').on('click', function() {
-    if ($(this).hasClass('active')) {
-      $(this).removeClass('active');
+  $('.cats-item-filled').on('click', function(event) {
+    event.preventDefault();
+
+    let filled = $(this);
+
+    if (filled.hasClass('active')) {
+      filled.removeClass('active');
+      $(this).closest('.cats-list__item').removeClass('on-active');
     } else {
-      $(this).addClass('active');
+      filled.addClass('active');
+      $(this).closest('.cats-list__item').addClass('on-active');
     }
 
-    if ($(this).hasClass('disabled')) {
+    if ($(this).closest('.cats-list__item').hasClass('disabled')) {
       $(this).removeClass('active');
       $(this).unbind('focus');
+      $(this).closest('.cats-list__item').removeClass('on-active');
     }
-
   });
+
+  $('.cats-list__message_link').on('click', function(e) {
+    e.preventDefault();
+    $(this).closest('.cats-list__item').addClass('on-active');
+    $(this).closest('.cats-list__item').find('.cats-item-filled').addClass('active');
+  });
+
+  // window.addEventListener('resize', function() {
+  //   sliderActivated();
+  // });
+  // sliderActivated();
+  //
+  // function sliderActivated() {
+  //   if ($(window).width() < 1200) {
+  //     $('.cats-list').slick({
+  //       infinite: true,
+  //       slidesToScroll: 1,
+  //       arrow: true
+  //     });
+  //   } else {
+  //     if ($('.cats-list').hasClass('slick-initialized')) {
+  //       $('.cats-list').slick('destroy');
+  //     }
+  //   }
+  // }
 });
